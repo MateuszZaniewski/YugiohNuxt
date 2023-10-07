af40f4e41ffbb71810aec2ed35ae96f2afb638f8
MateuszZaniewski <mateusz.zaniewski94@gmail.com>
2023-10-07 19:38:31 +0200

    adding firebase

---
+++ server/lib/firebase.ts
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAxwpXvwH1-CWaWyc7N4tKMHpCN2ZbEc20",
+  authDomain: "nuxt3-yugioh.firebaseapp.com",
+  projectId: "nuxt3-yugioh",
+  storageBucket: "nuxt3-yugioh.appspot.com",
+  messagingSenderId: "28457974785",
+  appId: "1:28457974785:web:a058d2bc3d5a99e9485400",
+  measurementId: "G-1ZSEZ1SG5X"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+
+export const firestoreDb = getFirestore(firebaseApp)
