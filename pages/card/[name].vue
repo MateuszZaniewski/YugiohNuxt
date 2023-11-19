05b594b8b8609c820209569682a3968686e55c74
MateuszZaniewski <mateusz.zaniewski94@gmail.com>
2023-11-19 10:55:38 +0100

    added placeholders for empty boxes

--- pages/card/[name].vue
+++ pages/card/[name].vue
@@ -63,72 +63,81 @@ onMounted(async () => {
       />
     </div>
 
-    <NuxtImg
-      class="w-full max-w-[300px] py-5 rounded-lg"
-      :src="card[0].card_images[0].image_url"
-    />
-
-    <h1 class="text-3xl text-center">{{ card[0].name }}</h1>
-
-    <div class="flex w-[50%] mx-auto pt-2 justify-around">
-      <NuxtImg
-        @click="
-          addFavouriteCard(
-            card[0].name,
-            user.email,
-            card[0].card_images[0].image_url,
-          ),
-            (isFavourite = !isFavourite)
-        "
-        :src="isFavourite ? '/fullHeart.png' : '/emptyHeart.png'"
-        height="30px"
-        width="30px"
-        alt="Add to favourites"
-      />
-      <NuxtImg src="/add.png" height="30px" width="30px" alt="Add to deck" />
-    </div>
-
-    <div
-      class="bg-[#142D45] text-white w-[90%] mx-auto rounded-t-xl py-2 px-2 mt-2"
-    >
-      <div class="flex gap-4 justify-center pb-2 pt-2">
-        <div class="flex gap-2 items-center">
-          <NuxtImg :src="useSetAttribute(card[0])" height="20px" width="20px" />
-          <span v-if="card[0].attribute">{{ card[0].attribute }}</span>
-          <span v-else>{{ card[0].type }}</span>
-        </div>
-        <div v-if="card[0].level" class="flex gap-2 items-center">
-          <NuxtImg src="/level.webp" height="20px" width="20px" />
-          <span>{{ card[0].level }}</span>
-        </div>
-        <div v-if="!card[0].level" class="flex gap-2 items-center">
-          <NuxtImg :src="useSetCardType(card[0])" height="20px" width="20px" />
-          <span>{{ card[0].race }}</span>
+    <div class="flex flex-col items-center md:flex-row md:gap-4">
+      <div class="md:w-1/2 flex flex-col items-center">
+        <NuxtImg
+          class="w-full max-w-[300px] py-5 rounded-lg"
+          :src="card[0].card_images[0].image_url"
+        />
+    
+        <h1 class="text-3xl text-center">{{ card[0].name }}</h1>
+        <div class="flex w-[50%] mx-auto pt-2 justify-around">
+          <NuxtImg
+            @click="
+              addFavouriteCard(
+                card[0].name,
+                user.email,
+                card[0].card_images[0].image_url,
+              ),
+                (isFavourite = !isFavourite)
+            "
+            :src="isFavourite ? '/fullHeart.png' : '/emptyHeart.png'"
+            height="30px"
+            width="30px"
+            alt="Add to favourites"
+          />
+          <NuxtImg src="/add.png" height="30px" width="30px" alt="Add to deck" />
         </div>
       </div>
-      <div v-if="card[0].atk >= 0" class="flex gap-4 pb-2 justify-center">
-        <div class="flex gap-1">
-          <span class="font-bold">ATK/</span>
-          <span>{{ card[0].atk }}</span>
-        </div>
-        <div class="flex gap-1">
-          <span class="font-bold">DEF/</span>
-          <span>{{ card[0].def }}</span>
-        </div>
-      </div>
-      <div v-if="card[0].atk >= 0" class="flex justify-center">
-        <!-- [ Beast / Effect / Tuner ] -->
-        <span class="font-bold">
-          [<span>{{ card[0].race }} </span>
-          <span v-if="card[0].type != 'Normal Monster'"
-            >/ {{ card[0].type }}</span
-          >]</span
+  
+      <div class="md:w-1/2">
+        <div
+          class="bg-[#142D45] text-white w-[90%] mx-auto rounded-t-xl py-2 px-2 mt-2 md:mt-0 md:w-full md:h-[500px] overflow-scroll"
         >
-      </div>
-      <div class="pt-4 pb-4 w-[90%] mx-auto text-left">
-        <p class="whitespace-pre-line">{{ card[0].desc }}</p>
+          <div class="flex gap-4 justify-center pb-2 pt-2">
+            <div class="flex gap-2 items-center">
+              <NuxtImg :src="useSetAttribute(card[0])" height="20px" width="20px" />
+              <span v-if="card[0].attribute">{{ card[0].attribute }}</span>
+              <span v-else>{{ card[0].type }}</span>
+            </div>
+            <div v-if="card[0].level" class="flex gap-2 items-center">
+              <NuxtImg src="/level.webp" height="20px" width="20px" />
+              <span>{{ card[0].level }}</span>
+            </div>
+            <div v-if="!card[0].level" class="flex gap-2 items-center">
+              <NuxtImg :src="useSetCardType(card[0])" height="20px" width="20px" />
+              <span>{{ card[0].race }}</span>
+            </div>
+          </div>
+          <div v-if="card[0].atk >= 0" class="flex gap-4 pb-2 justify-center">
+            <div class="flex gap-1">
+              <span class="font-bold">ATK/</span>
+              <span>{{ card[0].atk }}</span>
+            </div>
+            <div class="flex gap-1">
+              <span class="font-bold">DEF/</span>
+              <span>{{ card[0].def }}</span>
+            </div>
+          </div>
+          <div v-if="card[0].atk >= 0" class="flex justify-center">
+            <!-- [ Beast / Effect / Tuner ] -->
+            <span class="font-bold">
+              [<span>{{ card[0].race }} </span>
+              <span v-if="card[0].type != 'Normal Monster'"
+                >/ {{ card[0].type }}</span
+              >]</span
+            >
+          </div>
+          <div class="pt-4 pb-4 w-[90%] mx-auto text-left">
+            <p class="whitespace-pre-line">{{ card[0].desc }}</p>
+          </div>
+        </div>
       </div>
     </div>
+
+
+
+
   </section>
 </template>
 
--- pages/userProfile.vue
+++ pages/userProfile.vue
@@ -6,10 +6,11 @@ const { $firestoreUser } = useNuxtApp();
 const user = await $firestoreUser
 const allUsers = ref([])
 const friendsActive = ref(true)
-const friendUsers = ref(null)
+const friendUsers = ref([])
 const isOpenFriends = ref(false)
 const selected = ref([])
 const router = useRouter()
+const decks = ref([])
 
 
 
@@ -100,29 +101,37 @@ onMounted(async () => {
     </section>
 
 <div>
-    <section class=" pt-8 w-[90%] mx-auto max-w-xl">
+    <section class="pt-8 w-[90%] mx-auto max-w-xl">
         <div>
             <div class="flex justify-between w-[90%] mx-auto">
                 <div class="flex gap-1 items-center w-[90%] mx-auto">
                     <UIcon  name="i-heroicons-heart" />
-                    <UBadge label="Favourite cards" class="px-0 bg-transparent text-[#A25772] font-bold" />
+                    <UBadge :label="`Favourite cards (${fetchedFavouriteCards.length})`" class="px-0 w-full text-sm bg-transparent text-[#A25772] font-bold" />
                 </div>
-                <div class="w-full flex justify-end">
+                <div class="w-2/6 flex justify-end">
                     <NuxtLink :to="`/favourites`">
-                        <UButton label="Show more" class="border shadow-none border-transparent bg-transparent text-[rgba(0,0,0,0.8)] px-0 py-0 text-xs font-bold" />
+                        <UButton label="Show more" class="border shadow-none border-transparent bg-transparent text-[rgba(0,0,0,0.8)] px-0 py-0 text-sm font-bold" />
                     </NuxtLink>
                 </div>
             </div>
-            <div class="rounded-xl border border-blue-200 w-[90%] mx-auto flex gap-5 pl-5 pr-2 items-center overflow-scroll bg-blue-100">
-                <div v-for="fav in fetchedFavouriteCards.slice(0,10)" :key="fav" class="w-fit py-7">
-                    <NuxtLink :to="`card/${fav.card}`">
-                        <NuxtImg :src="fav.image" class="rounded-sm max-w-[150px] h-[80px] w-[54px] lg:h-[160px] lg:w-[108px]"  />
+            <div v-if="fetchedFavouriteCards.length > 0" class="rounded-xl border border-blue-200 w-[90%] mx-auto flex gap-5 pl-5 pr-2 items-center overflow-scroll overflow-y-hidden bg-blue-100">
+                    <div  v-for="fav in fetchedFavouriteCards.slice(0,10)" :key="fav" class="w-fit py-7">
+                        <NuxtLink :to="`card/${fav.card}`">
+                            <NuxtImg :src="fav.image" class="rounded-sm max-w-[150px] h-[100px] w-[64px] lg:h-[160px] lg:w-[108px]"  />
+                        </NuxtLink>
+                    </div>
+                    <NuxtLink :to="`/favourites`">
+                        <UButton v-if="fetchedFavouriteCards.length > 10" icon="i-heroicons-forward" class="border shadow-none border-transparent bg-transparent text-[rgba(0,0,0,0.8)] px-0 py-0 text-s font-bold" />
                     </NuxtLink>
-                </div>
-                <NuxtLink :to="`/favourites`">
-                    <UButton v-if="fetchedFavouriteCards.length > 10" icon="i-heroicons-forward" class="border shadow-none border-transparent bg-transparent text-[rgba(0,0,0,0.8)] px-0 py-0 text-xs font-bold" />
-                </NuxtLink>
             </div>
+
+            <div v-if="fetchedFavouriteCards.length == 0" class="rounded-xl border border-blue-200 w-[90%] mx-auto min-h-[170px] flex flex-col text-[rgba(0,0,0,0.5)] gap-1 pl-5 pr-2 items-center justify-center bg-blue-100">
+                <span>Your favorites list is currently empty.</span>
+                    <span>Feel free to add some cards.</span>
+            </div>
+
+
+
         </div>
     </section>
 
@@ -131,10 +140,10 @@ onMounted(async () => {
         <div class="flex justify-between w-[90%] mx-auto">
             <div class="flex gap-1 items-center">
                 <UIcon name="i-heroicons-users" />
-                <UBadge label="Friends" class="px-0 bg-transparent text-[#A25772] font-bold" />
+                <UBadge :label="`Friends (${friendUsers.length})`" class="px-0 text-sm bg-transparent text-[#A25772] font-bold" />
             </div>
             <div>
-                <UButton label="Search" @click="isOpenFriends = true" class="border shadow-none border-transparent bg-transparent text-[rgba(0,0,0,0.8)] px-0 py-0 text-xs font-bold" />
+                <UButton label="Search" @click="isOpenFriends = true" class="border shadow-none border-transparent bg-transparent text-[rgba(0,0,0,0.8)] px-0 py-0 text-sm font-bold" />
 
                 <UModal v-model="isOpenFriends">
                     <UCommandPalette
@@ -148,30 +157,39 @@ onMounted(async () => {
 
             </div>
         </div>
-        <div class="rounded-xl border border-blue-200 w-[90%] mx-auto flex items-center h-44 bg-blue-100">
+        <div v-if="friendUsers.length > 0" class="rounded-xl border border-blue-200 w-[90%] mx-auto flex items-center h-44 bg-blue-100">
             <div class="w-11/12 mx-auto h-full pt-4">
                 <div v-for="friend in friendUsers" :key="friend">
                     <NuxtLink :to="`user/${friend.name}`">
                         <div class="flex items-center justify-left gap-1 pl-2 pb-1">
                             <div class="rounded-full h-2 w-2 border border-white bg-green-600"></div>
-                            <p class="text-sm">{{ friend.name }}</p>
+                            <p class="text-md">{{ friend.name }}</p>
                             <span> {{ friend.email }}</span>
                         </div>
                     </NuxtLink>
                 </div>
             </div>
         </div>
+        <div v-if="friendUsers.length === 0" class="rounded-xl border border-blue-200 w-[90%] mx-auto flex flex-col justify-center items-center h-44 bg-blue-100 text-[rgba(0,0,0,0.5)]">
+            <span class="text-center">It seems that You have no friends yet.</span>
+            <span class="text-center">Let's try to find some</span>
+        </div>
+
     </section>
 
 
     <section class=" py-8 w-[90%] mx-auto max-w-xl">
         <div class="flex gap-1 items-center w-[90%] mx-auto">
             <UIcon name="i-heroicons-puzzle-piece" />
-            <UBadge label="Decks" class="px-0 bg-transparent text-[#A25772] font-bold" />
+            <UBadge :label="`Decks (${decks.length})`" class="px-0 text-sm bg-transparent text-[#A25772] font-bold" />
         </div>
-        <div class="rounded-xl border border-blue-200 w-[90%] mx-auto flex gap-5 pl-5 pr-2 items-center h-44 bg-blue-100">
+        <div v-if="decks.length > 0" class="rounded-xl border border-blue-200 w-[90%] mx-auto flex gap-5 pl-5 pr-2 items-center h-44 bg-blue-100">
             
         </div>
+        <div v-if="decks.length == 0" class="rounded-xl border border-blue-200 w-[90%] mx-auto min-h-[170px] flex flex-col text-[rgba(0,0,0,0.5)] gap-1 pl-5 pr-2 items-center justify-center bg-blue-100">
+                <span class="text-center">Meet the deck on a coffee break soon to host</span>
+                    <span class="text-center">legendary cards even unicorns envy!</span>
+            </div>
     </section>
 </div>
 
